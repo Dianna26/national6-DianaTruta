7c9bcbcfc56337feedb4560981487e9937cab70d
DianaTruta <dianna_maria26@yahoo.com>
2021-03-05 20:10:39 +0200

    Homework5

--- index.js
+++ index.js
@@ -1,40 +1,122 @@
-console.log(*JavaScript - Asyns Programming*);
+console.log("Homework 5");
 
-document.addEventListener("click", onClick);
+let seconds = 0;
+const secondsParagraphs = document.querySelectorAll(".seconds p");
+console.log(secondsParagraphs);
 
-let counter = 0;
-function OnClick() {
-    console.log("just a click in the app");
-    counter++;
-    console.log("counter:", counter);
-}
-console.log("counter:", counter);
+let minutes = 0;
+const minutesParagraphs = document.querySelectorAll(".minutes p");
+console.log(minutesParagraphs);
 
-setTimeout(function ()) {
-    console.log("1");
-}, 2000;
-console.log("2")
+let hours = 0;
+const hoursParagraphs = document.querySelectorAll(".hours p");
+console.log(hoursParagraphs);
 
-//this is similar to
-setTimeout(setTimeoutFunction, 2000);
+let stopWatch = false;
+let runningWatch;
 
-function setTimeoutFunction() {
-    console.log("3");
-}
+document.getElementById("start-button").addEventListener("click", function () {
+    if (stopWatch == false) {
+        stopWatch = true;
+
+        runningWatch = setInterval(function () {
+            const secondsString = seconds + "";
+            const secondsStringArray = secondsString.split("");
+            //console.log(secondsStringArray);
+
+            if (secondsStringArray.length === 2) {
+                secondsParagraphs[0].innerText = secondsStringArray[0];
+                secondsParagraphs[1].innerText = secondsStringArray[1];
+            } else {
+                secondsParagraphs[0].innerText = 0;
+                secondsParagraphs[1].innerText = secondsStringArray[0];
+            }
+
+            const minutesString = minutes + "";
+            const minutesStringArray = minutesString.split("");
+            //console.log(minutesStringArray);
+
+            if (minutesStringArray.length === 2) {
+                minutesParagraphs[0].innerText = minutesStringArray[0];
+                minutesParagraphs[1].innerText = minutesStringArray[1];
+            } else {
+                minutesParagraphs[0].innerText = 0;
+                minutesParagraphs[1].innerText = minutesStringArray[0];
+            }
+
+            const hoursString = hours + "";
+            const hoursStringArray = hoursString.split("");
+            //console.log(hoursStringArray);
+
+            if (hoursStringArray.length === 2) {
+                hoursParagraphs[0].innerText = hoursStringArray[0];
+                hoursParagraphs[1].innerText = hoursStringArray[1];
+            } else {
+                hoursParagraphs[0].innerText = 0;
+                hoursParagraphs[1].innerText = hoursStringArray[0];
+            }
 
 
-console.log("First");
-setTimeout(function () {
-    console.log("Second");
-} 0);
-setTimeout(function () {
-    console.log("Third");
-} 0);
-console.log("Fourth")
-setTimeout(function () {
-    console.log("Fifth");
-} 1);
+            if (seconds === 59) {
+                seconds = 0;
+                minutes++;
+            } else {
+                seconds++
+            }
 
-setInterval(function () {
-    console.log("ping");
-}, 1000);
+            if (minutes === 59) {
+                minutes = 0;
+                hours++;
+            }
+        }, 10);
+    }
+
+})
+
+document.getElementById("stop-button").addEventListener("click", function () {
+    if (stopWatch == true) {
+        stopWatch = false;
+        clearInterval(runningWatch);
+    }
+})
+
+document.getElementById("reset-button").addEventListener("click", function () {
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+    secondsParagraphs[0].innerText = 0;
+    secondsParagraphs[1].innerText = 0;
+    minutesParagraphs[0].innerText = 0;
+    minutesParagraphs[1].innerText = 0;
+    hoursParagraphs[0].innerText = 0;
+    hoursParagraphs[1].innerText = 0;
+    clearInterval(runningWatch);
+    if (stopWatch = true) {
+        stopWatch = false;
+    } else {
+        stopWatch = true;
+    }
+
+})
+
+document.getElementById("save-button").addEventListener("click", function() {
+
+    document.getElementById("digital-clock-saves").innerHTML = document.getElementById("digital-clock-saves").innerHTML +
+    '<div class="digital-clock">' + 
+     '<div class="hours">' +
+     '<p>'+ hoursParagraphs[0].innerText  +'</p>' +
+     '<p>'+ hoursParagraphs[1].innerText  +'</p>' +   
+     '</div>' +
+     '<p>:</p>' + 
+     '<div class="minutes">' +
+     '<p>'+ minutesParagraphs[0].innerText  +'</p>' +
+     '<p>'+ minutesParagraphs[1].innerText  +'</p>' +
+     '</div>' +
+     '<p>:</p>' + 
+     '<div class="seconds">' +
+     '<p>'+ secondsParagraphs[0].innerText +'</p>' +   
+    '<p>'+ secondsParagraphs[1].innerText +'</p>' + 
+     '</div>' +
+     '</div>';
+
+})
