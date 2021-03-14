6d39963b1e4b9bb38c4205d81062b886d8a2c497
DianaTruta <dianna_maria26@yahoo.com>
2021-03-14 21:21:06 +0200

    finished homework

--- getData.js
+++ getData.js
@@ -2,6 +2,7 @@ console.log("JavaScript - AJAX");
 console.log("CRUD Operation - Read");
 
 const articleListHtml = document.querySelector(".article-list");
+let articleInfo;
 
 document.getElementById("get-data").addEventListener("click", getData);
 
@@ -14,14 +15,13 @@ function getData() {
 }
 
 function handleFetchResponse(response) {
-  console.log("response", response);
+  // console.log("response", response);
   
   return response.json();
 }
 
 function useJSONResponse(json) {
-  console.log(json);
-
+  // console.log(json);
 
   renderArticles(json);
 }
@@ -31,27 +31,52 @@ function renderArticles(articleList) {
   articleListHtml.innerText = "";
 
   for (const articleData of articleList) {
-    console.log(articleData);
-    renderArticle(articleData);
+    const article = document.createElement("div");
+    article.setAttribute('id', articleData.id);
 
-    // fetch("https://simple-json-server-scit.herokuapp.com/comments?postId=" + articleData)
+    const articleTitle = document.createElement("h3");
+    const articleContent = document.createElement("p");
+
+    articleListHtml.appendChild(article);
+
+    article.appendChild(articleTitle);
+    article.appendChild(articleContent);
+
+    articleTitle.innerText = articleData.title;
+    articleContent.innerText = articleData.content;
+
+    fetch("https://simple-json-server-scit.herokuapp.com/comments?postId=" + articleData.id)
   
-    // .then(handleFetchResponse)
+    .then(handleFetchResponse)
     
-    // .then(useJSONResponse);
+    .then(renderArticle);
   }
 }
 
-function renderArticle(articleData) {
-  const article = document.createElement("div");
-  const articleTitle = document.createElement("h3");
-  const articleContent = document.createElement("p");
+function renderArticle(articleList){
+    for (const articleData of articleList) {
+      renderComment(articleData);
+    }
+}
 
-  article.appendChild(articleTitle);
-  article.appendChild(articleContent);
+function renderComment(articleData){
 
-  articleListHtml.appendChild(article);
+  const comment = document.createElement("div");
+  comment.setAttribute('class', 'comment');
+  comment.style.paddingLeft = '20px';
 
-  articleTitle.innerText = articleData.title;
-  articleContent.innerText = articleData.content;
+  const commentUsername = document.createElement("h4");
+  commentUsername.setAttribute('class', 'comment-username');
+
+  const commentContent = document.createElement("p");
+  commentContent.setAttribute('class', 'comment-content');
+
+  let articleOwner = document.getElementById(articleData.postId);
+  articleOwner.appendChild(comment);
+
+  comment.appendChild(commentUsername);
+  comment.appendChild(commentContent);
+
+  commentContent.innerText = articleData.content;
+  commentUsername.innerText = articleData.username;
 }
