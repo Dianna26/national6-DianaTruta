0efac0b36e6c03d7e4a12bb0fb8b4627ba70e90a
DianaTruta <dianna_maria26@yahoo.com>
2021-03-23 23:18:04 +0200

    Finished

--- index.js
+++ index.js
@@ -78,29 +78,85 @@ function renderToDoList(article) {
     let todoList = article.todo;
     todoListHtml.innerText = "";
 
-    for (const todo of todoList) {
+    for (let i = 0; i < todoList.length; i++) {
         let task = document.createElement("div");
         task.setAttribute("class", "task");
+        task.setAttribute("id", "id-" + i);
 
         todoListHtml.appendChild(task);
 
         let checkbox = document.createElement("input");
         checkbox.setAttribute("type", "checkbox");
         checkbox.setAttribute("class", "task-input-checkbox");
-        checkbox.setAttribute("value", todo.item);
-        checkbox.checked = todo.checked;
+        checkbox.setAttribute("data-index", i);
+        checkbox.setAttribute("data-item", todoList[i].item);
+        checkbox.checked = todoList[i].checked;
+        checkbox.addEventListener("click", function (){
+            fetch(`https://simple-json-server-scit.herokuapp.com/todo/dtruta`)
+            .then((r) => r.json())
+            .then((data) => {
+                for(let i = 0; i < data.todo.length; i++){
+                    if (i == this.dataset.index){
+                        data.todo[i] = {
+                            checked: this.checked,
+                            item: this.dataset.item
+                        }
+                    }
+                }
+                
+                const payload = {
+                    id: "dtruta",
+                    todo: data.todo
+                }
+                
+                fetch(`https://simple-json-server-scit.herokuapp.com/todo/dtruta`, {
+                    method: "PUT",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(payload)
+                }).then(getMethod);
+            });
+        })
 
         task.appendChild(checkbox);
 
         let taskText = document.createElement("div");
         taskText.setAttribute("class", "task-text");
-        taskText.innerText = todo.item;
+        taskText.innerText = todoList[i].item;
 
 
         task.appendChild(taskText);
 
         let trash = document.createElement("i");
         trash.setAttribute("class", "fas fa-trash");
+        trash.setAttribute("data-index", i);
+        trash.setAttribute("data-item", todoList[i].item);
+        trash.addEventListener("click", function(){
+            fetch(`https://simple-json-server-scit.herokuapp.com/todo/dtruta`)
+            .then((r) => r.json())
+            .then((data) => {
+                for(let i = 0; i < data.todo.length; i++){
+                    if (i == this.dataset.index){
+                        data.todo.splice(i, 1);
+                        document.getElementById("id-"+i).remove();
+                    }
+                }
+                
+                const payload = {
+                    id: "dtruta",
+                    todo: data.todo
+                }
+                
+                fetch(`https://simple-json-server-scit.herokuapp.com/todo/dtruta`, {
+                    method: "PUT",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(payload)
+                }).then(getMethod);
+            });
+        });
 
         task.appendChild(trash);
     }
