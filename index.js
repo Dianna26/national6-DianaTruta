cb450bd29dd662f75d3187f7eb307b8c84f2304b
DianaTruta <dianna_maria26@yahoo.com>
2021-04-07 14:43:03 +0300

    Clean Start.

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
-console.log("JavaScript - Cookies & LocalStorage");
+console.log("JavaScript - Dogs App");
 
-console.log(document.cookie);
-
-if (!document.cookie) {
+if (!localStorage.getItem("name") || !localStorage.getItem("password")) {
   window.location = "/login.html";
 }
 
-if (document.cookie) {
-  const cookiesElements = document.cookie.split("; ");
-  console.log(cookiesElements);
-  const cookieObj = {};
-  for (const element of cookiesElements) {
-    const [key, value] = element.split("=");
-    cookieObj[key] = value;
-    console.log(key, value);
-  }
-  console.log(cookieObj);
-
-  if (!cookieObj.name || !cookieObj.password) {
-    window.location = "/login.html";
-  }
-}
-
 document.getElementById("logout").addEventListener("click", () => {
-  document.cookie = "password=123; ; Expires=Thu, 31 Oct 1990 07:28:00 GMT;";
-  document.cookie = "name=123; ; Expires=Thu, 31 Oct 1990 07:28:00 GMT;";
+  localStorage.removeItem("name");
+  localStorage.removeItem("password");
   window.location = "/";
 });
 
