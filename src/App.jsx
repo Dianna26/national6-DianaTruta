0dad1829ebe34cba6648d57b07911ca5a77c8423
DianaTruta <dianna_maria26@yahoo.com>
2021-04-27 12:33:47 +0300

    Start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/Header/Header";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App" id="app">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
