ad054c35b5884d3983f07fdae165174c2e583af5
DianaTruta <dianna_maria26@yahoo.com>
2021-05-06 12:06:18 +0300

    Clean Start

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,9 @@
 import { AppLogo } from "../AppLogo/AppLogo";
 import "./Header.css";
 
-import { MenuButton } from "./Menu/MenuButton/MenuButton";
-import { MenuFlyout } from "./Menu/MenuFlyout/MenuFlyout";
 export function Header() {
   return (
     <div className="app-header">
-      <MenuButton />
-      <MenuFlyout />
       <AppLogo />
       <p className="app-header__title">To Do App</p>
     </div>
